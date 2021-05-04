b9d4815e94522d639a2d6cd65e2429f5f3c65043
RamirezInformation <ramirezinformation@gmail.com>
2021-05-04 01:30:10 -0400

    introduccion de lighbox

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var urlImagen;
 
 var fileButton = document.getElementById("dato9");
 //vigilar foto
-fileButton.addEventListener('change', function (e) {
+fileButton.addEventListener('change', function(e) {
     console.log("holllllllla");
     //obtener archivo
     var file = e.target.files[0];
@@ -21,21 +21,35 @@ fileButton.addEventListener('change', function (e) {
 
 })
 
-
+//actualiza la galeria al detectar cambios
 var starCountRef = firebase.database().ref('mascotas/');
 starCountRef.on('child_added', (snapshot) => {
-    $("#galeria").append("<div class='foto'><img class='img-hover' src='" + snapshot.val().fotografia + "'><div class='oculto'><h4>Mascota 1</h4></div></div>");
+    var elem1 = "<div class='foto'><img class='img-hover' src='" + snapshot.val().fotografia + "'>";
+    var elem2 = "<div class='oculto'><a class='prueba' title='<h2>" + snapshot.val().estado + "</h2>' href=" + snapshot.val().fotografia + " data-lightbox='roadtrip'>";
+    var elem3 = "<img class='img-hover' src=" + snapshot.val().fotografia + " alt=''></a></div></div>";
+    $("#galeria").append(elem1 + elem2 + elem3);
+    $(document).ready(function() {
+        $('.foto').hover(function() {
+            $(this).find('.oculto').fadeIn();
+            $(this).find('.img-hover').addClass('agrandar');
+        }, function() {
+            $(this).find('.oculto').fadeOut();
+            $(this).find('.img-hover').removeClass('agrandar');
+        })
+
+
+    })
 });
 
-
-(function () {
+//obtiene los datos del formulario
+(function() {
     'use strict';
-    window.addEventListener('load', function () {
+    window.addEventListener('load', function() {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
@@ -70,7 +84,7 @@ starCountRef.on('child_added', (snapshot) => {
 })();
 
 
-$('#login').click(function () {
+$('#login').click(function() {
     firebase.auth()
         .signInWithPopup(provider)
         .then((result) => {
@@ -86,6 +100,7 @@ $('#login').click(function () {
         });
 
 })
+
 function guardarDatos(user) {
     var usuario = {
         uid: user.uid,
@@ -95,32 +110,41 @@ function guardarDatos(user) {
     }
     firebase.database().ref("usuarios/" + user.uid).set(usuario)
 }
-$('#loginOut').click(function () {
+$('#loginOut').click(function() {
     firebase.auth().signOut()
-        .then(function () {
+        .then(function() {
             console.log('Salir');
             $('#loginOut').hide();
             $('#iconFoto').hide();
             $('#login').show();
             $('.registroOculto').hide();
         })
-        .catch(function (error) {
+        .catch(function(error) {
             console.log('error');
         })
 
 })
 
 
-$(document).ready(function () {
-    $('.foto').hover(function () {
-        $(this).find('.oculto').fadeIn();
-        $(this).find('.img-hover').addClass('agrandar');
-    }, function () {
-        $(this).find('.oculto').fadeOut();
-        $(this).find('.img-hover').removeClass('agrandar');
-    })
-    $('.menu-icon').click(function () {
-        $('nav').slideToggle();
-    })
 
+$('.menu-icon').click(function() {
+    $('nav').slideToggle();
+})
+
+
+
+$('#tipoRegistro1').click(function() {
+    document.getElementById("tipoRegistro1").className = "btn btn-success btn-lg active";
+    document.getElementById("tipoRegistro2").className = "btn btn-secondary btn-lg active";
+    document.getElementById("tipoRegistro3").className = "btn btn-secondary btn-lg active";
+})
+$('#tipoRegistro2').click(function() {
+    document.getElementById("tipoRegistro1").className = "btn btn-secondary btn-lg active";
+    document.getElementById("tipoRegistro2").className = "btn btn-success btn-lg active";
+    document.getElementById("tipoRegistro3").className = "btn btn-secondary btn-lg active";
+})
+$('#tipoRegistro3').click(function() {
+    document.getElementById("tipoRegistro1").className = "btn btn-secondary btn-lg active";
+    document.getElementById("tipoRegistro2").className = "btn btn-secondary btn-lg active";
+    document.getElementById("tipoRegistro3").className = "btn btn-success btn-lg active";
 })
