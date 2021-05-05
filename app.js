751d3287dfa84435b60ca602beac48f8ee0e05d9
RamirezInformation <ramirezinformation@gmail.com>
2021-05-05 00:10:49 -0400

    mejora en la autentificacion

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        console.log('user logged in: ', user);
+        //$('#menuRegistro').append(" <li><img src='" +user.photoURL+ "' id='iconFoto'/></li>");
+        $('#login').hide();
+        $('#loginOut').show();
+        $('#fotoGoogle').append(" <img src='" + user.photoURL + "' id='iconFoto'/>");
+        $('.info').append("<p><i id='nombreUsuario' class='fas fa-phone-alt'></i>   " + user.displayName + "</p>");
+        $('.info').append("<p><i id='emailUsuario' class='far fa-envelope'></i>   " + user.email + "</p>");
+        $('.registroOculto').show();
+        usuario = user;
+        guardarDatos(user);
+
+    } else {
+        console.log('user logged out');
+        $('#loginOut').hide();
+        $('#iconFoto').hide();
+        $('#login').show();
+        $('.registroOculto').hide();
+
+    }
+})
+
+
+
 //login
 var provider = new firebase.auth.GoogleAuthProvider();
 var usuario;
@@ -5,7 +30,7 @@ var urlImagen;
 
 var fileButton = document.getElementById("dato9");
 //vigilar foto
-fileButton.addEventListener('change', function(e) {
+fileButton.addEventListener('change', function (e) {
     console.log("holllllllla");
     //obtener archivo
     var file = e.target.files[0];
@@ -28,11 +53,11 @@ starCountRef.on('child_added', (snapshot) => {
     var elem2 = "<div class='oculto'><a class='prueba' title='<h2>" + snapshot.val().estado + "</h2>' href=" + snapshot.val().fotografia + " data-lightbox='roadtrip'>";
     var elem3 = "<img class='img-hover' src=" + snapshot.val().fotografia + " alt=''></a></div></div>";
     $("#galeria").append(elem1 + elem2 + elem3);
-    $(document).ready(function() {
-        $('.foto').hover(function() {
+    $(document).ready(function () {
+        $('.foto').hover(function () {
             $(this).find('.oculto').fadeIn();
             $(this).find('.img-hover').addClass('agrandar');
-        }, function() {
+        }, function () {
             $(this).find('.oculto').fadeOut();
             $(this).find('.img-hover').removeClass('agrandar');
         })
@@ -42,20 +67,20 @@ starCountRef.on('child_added', (snapshot) => {
 });
 
 //obtiene los datos del formulario
-(function() {
+(function () {
     'use strict';
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
+
                 }
                 if (form.checkValidity() === true) {
-
                     var mascota = {
                         tipo: document.getElementById("dato1").value,
                         raza: document.getElementById("dato2").value,
@@ -76,6 +101,7 @@ starCountRef.on('child_added', (snapshot) => {
                         telefono: document.getElementById("dato13").value
                     }
                     firebase.database().ref("mascotas/").push(mascota)
+
                 }
                 form.classList.add('was-validated');
             }, false);
@@ -84,19 +110,11 @@ starCountRef.on('child_added', (snapshot) => {
 })();
 
 
-$('#login').click(function() {
+$('#login').click(function () {
     firebase.auth()
         .signInWithPopup(provider)
         .then((result) => {
-            console.log(result.user);
-            $('#login').hide();
-            $('#loginOut').show();
-            $('#fotoGoogle').append(" <img src='" + result.user.photoURL + "' id='iconFoto'/>");
-            $('.info').append("<p><i id='nombreUsuario' class='fas fa-phone-alt'></i>   " + result.user.displayName + "</p>");
-            $('.info').append("<p><i id='emailUsuario' class='far fa-envelope'></i>   " + result.user.email + "</p>");
-            $('.registroOculto').show();
             usuario = result.user;
-            guardarDatos(result.user);
         });
 
 })
@@ -110,16 +128,12 @@ function guardarDatos(user) {
     }
     firebase.database().ref("usuarios/" + user.uid).set(usuario)
 }
-$('#loginOut').click(function() {
+$('#loginOut').click(function () {
     firebase.auth().signOut()
-        .then(function() {
+        .then(function () {
             console.log('Salir');
-            $('#loginOut').hide();
-            $('#iconFoto').hide();
-            $('#login').show();
-            $('.registroOculto').hide();
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log('error');
         })
 
@@ -127,24 +141,29 @@ $('#loginOut').click(function() {
 
 
 
-$('.menu-icon').click(function() {
+$('.menu-icon').click(function () {
     $('nav').slideToggle();
 })
 
 
 
-$('#tipoRegistro1').click(function() {
+$('#tipoRegistro1').click(function () {
     document.getElementById("tipoRegistro1").className = "btn btn-success btn-lg active";
-    document.getElementById("tipoRegistro2").className = "btn btn-secondary btn-lg active";
     document.getElementById("tipoRegistro3").className = "btn btn-secondary btn-lg active";
+    document.getElementById("tipoRegistro4").className = "btn btn-secondary btn-lg active";
+    $('#formulario1').show();
+    
 })
-$('#tipoRegistro2').click(function() {
+
+$('#tipoRegistro3').click(function () {
     document.getElementById("tipoRegistro1").className = "btn btn-secondary btn-lg active";
-    document.getElementById("tipoRegistro2").className = "btn btn-success btn-lg active";
-    document.getElementById("tipoRegistro3").className = "btn btn-secondary btn-lg active";
-})
-$('#tipoRegistro3').click(function() {
-    document.getElementById("tipoRegistro1").className = "btn btn-secondary btn-lg active";
-    document.getElementById("tipoRegistro2").className = "btn btn-secondary btn-lg active";
     document.getElementById("tipoRegistro3").className = "btn btn-success btn-lg active";
+    document.getElementById("tipoRegistro4").className = "btn btn-secondary btn-lg active";
+    $('#formulario1').hide();
+})
+$('#tipoRegistro4').click(function () {
+    document.getElementById("tipoRegistro1").className = "btn btn-secondary btn-lg active";
+    document.getElementById("tipoRegistro3").className = "btn btn-secondary btn-lg active";
+    document.getElementById("tipoRegistro4").className = "btn btn-success btn-lg active";
+    $('#formulario1').hide();
 })
